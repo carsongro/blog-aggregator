5d1deb9599451725953c5ff69e328370272b87d5
Carson Gross <carsongro@gmail.com>
2024-04-18 14:55:49 -0600

    Added APIKey to users

--- main.go
+++ main.go
@@ -54,6 +54,7 @@ func main() {
 	v1Router.Get("/healthz", handlerRediness)
 	v1Router.Get("/err", handlerErr)
 	v1Router.Post("/users", apiConfig.handlerCreateUser)
+	v1Router.Get("/users", apiConfig.handlerGetUser)
 
 	router.Mount("/v1", v1Router)
 
